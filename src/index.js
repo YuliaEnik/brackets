9590957dfb7aa696ded2089e4f35650392236a76
Yulia Aleinik <aleinikya@mail.ru>
2020-10-23 23:10:35 +0300

    Init:bracket task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+  let close = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+  let newArray = [];
+  let isAlreadyAdded = false;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      newArray.push(str[i]);
+      if (close.includes(str[i])) {
+        isAlreadyAdded = true;
+        count += 1;
+      }
+    } else {
+      if (close.indexOf(str[i]) === open.indexOf(newArray[newArray.length - 1])) {
+        newArray.pop();
+      } else {
+        return false;
+      }
+    }
+    if (isAlreadyAdded && count >= 2) {
+      if (newArray[newArray.length -1] === newArray[newArray.length - 2]) {
+        newArray.pop();
+        newArray.pop();
+        count -= 2;
+        if (count === 0) {
+          isAlreadyAdded = false;
+        }
+      } 
+    }
+  } return newArray.length === 0;
 }
